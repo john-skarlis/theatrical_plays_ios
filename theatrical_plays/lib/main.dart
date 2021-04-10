cbdb92fedf396d661bc313ef4826cb5fa382738d
John Skarlis <skarlisgiannis@gmail.com>
2021-04-10 15:27:56 +0300

    starting home

--- theatrical_plays/lib/main.dart
+++ theatrical_plays/lib/main.dart
@@ -5,8 +5,11 @@ import 'package:theatrical_plays/pages/home.dart';
 import 'package:theatrical_plays/pages/loading.dart';
 
 void main() {
-  runApp(MaterialApp(initialRoute: '/', routes: {
-    '/'(context) => Loading(),
-    '/home'(context)=> Home(),
-  }));
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+    },
+  ));
 }
