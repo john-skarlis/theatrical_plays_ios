408e37c313c6f2e28ae9b68bf1b7473ea8235e2b
John Skarlis <skarlisgiannis@gmail.com>
2021-04-10 20:19:35 +0300

    bottom nav

--- theatrical_plays/lib/pages/home.dart
+++ theatrical_plays/lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,9 +10,29 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(44, 44, 44, 1),
       appBar: AppBar(
-        title: Text('Theatrical plays'),
-        backgroundColor: Color.fromARGB(0, 29, 29, 29),
+        title: Text(
+          'Theatrical plays',
+          style: TextStyle(color: Color.fromRGBO(113, 255, 250, 1)),
+        ),
+        backgroundColor: Color.fromRGBO(29, 29, 29, 1),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Color.fromRGBO(44, 44, 44, 0),
+        color: Color.fromRGBO(29, 29, 29, 1),
+        buttonBackgroundColor: Color.fromRGBO(113, 255, 250, 1),
+        items: <Widget>[
+          Icon(
+            Icons.movie_outlined,
+            size: 30,
+          ),
+          Icon(Icons.home_outlined, size: 30),
+          Icon(Icons.person_outline, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
       ),
     );
   }
