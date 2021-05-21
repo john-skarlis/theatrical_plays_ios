4ceae41a68fd723c5073cba0ce4c8015a14047ba
John Skarlis <skarlisgiannis@gmail.com>
2021-05-21 15:54:27 +0300

    unused import

--- theatrical_plays/lib/pages/loading_actors.dart
+++ theatrical_plays/lib/pages/loading_actors.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart';
 import 'package:theatrical_plays/models/actor.dart';
 import 'package:theatrical_plays/pages/actors.dart';
